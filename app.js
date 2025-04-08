6b7666a66f3e3f9cf9dfa91ba9dbbfa3875aa57a
FraCav99 <mastercaster36@outlook.it>
2025-04-08 12:33:31 +0200

    logic: change svg title when popup is open and clicking outside

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const popupOpenerBtn = document.getElementById("cta-popup-opener-btn");
 const socialLinkPopup = document.getElementById("cta-popup");
-const buttonTextLabel = document.getElementById('social-media-popup-btn');
+const buttonTextLabel = document.getElementById("social-media-popup-btn");
 
 popupOpenerBtn.addEventListener("click", function () {
-  toggleSocialPopup(!socialLinkPopup.classList.contains('open'));
+  toggleSocialPopup(!socialLinkPopup.classList.contains("open"));
 });
 
 document.addEventListener("click", function (ev) {
@@ -11,11 +11,15 @@ document.addEventListener("click", function (ev) {
   if (!socialLinkPopup.contains(target) && !popupOpenerBtn.contains(target)) {
     socialLinkPopup.classList.remove("open");
     popupOpenerBtn.setAttribute("aria-expanded", false);
+    buttonTextLabel.textContent = buttonTextLabel.textContent.replace(
+      "Close",
+      "Open"
+    );
   }
 });
 
 document.addEventListener("keyup", function (ev) {
-  if (ev.key === "Escape" && socialLinkPopup.classList.contains('open')) {
+  if (ev.key === "Escape" && socialLinkPopup.classList.contains("open")) {
     toggleSocialPopup(false);
   }
 });
@@ -25,13 +29,13 @@ function toggleSocialPopup(shouldBeOpen) {
   popupOpenerBtn.setAttribute("aria-expanded", shouldBeOpen);
   if (shouldBeOpen) {
     buttonTextLabel.textContent = buttonTextLabel.textContent.replace(
-      'Open', 
-      'Close'
+      "Open",
+      "Close"
     );
   } else {
     buttonTextLabel.textContent = buttonTextLabel.textContent.replace(
-      'Close', 
-      'Open'
+      "Close",
+      "Open"
     );
   }
 }
